5aff6bb15c371f07d62fe2ee5ff255d336835d61
kwarp11 <kwarp11@c8177bcf-d549-e513-4c82-ab526b154d05>
2010-04-09 03:39:33 +0000

    unit test menu options are in

---
+++ trunk/monkey ball/SuperDragonBall/Screens/GregTestMenu.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GameStateManagement
+{
+    class GregTestMenu : MenuScreen
+    {
+
+        public GregTestMenu()
+            : base("Greg Unit Tests")
+        {
+
+            MenuEntry test1 = new MenuEntry("Test 1");
+            MenuEntry test2 = new MenuEntry("Test 2");
+      
+            MenuEntry exitMenuEntry = new MenuEntry("Back");
+
+            // Hook up menu event handlers.
+            test1.Selected += LaunchTest1;
+            test2.Selected += LaunchTest2;
+         
+            exitMenuEntry.Selected += OnCancel;
+
+            MenuEntries.Add(test1);
+            MenuEntries.Add(test2);
+       
+            MenuEntries.Add(exitMenuEntry);
+        }
+
+
+        /// <summary>
+        /// Event handler for when the Options menu entry is selected.
+        /// </summary>
+        void LaunchTest1(object sender, EventArgs e)
+        {
+           // LoadingScreen.Load(ScreenManager, true, new GameplayScreen());
+        }
+
+        /// <summary>
+        /// Event handler for when the Options menu entry is selected.
+        /// </summary>
+        void LaunchTest2(object sender, EventArgs e)
+        {
+           // LoadingScreen.Load(ScreenManager, true, new GameplayScreen());
+        }
+      
+
+
+
+    }
+}
---
+++ trunk/monkey ball/SuperDragonBall/Screens/UnitTestMenuScreen.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GameStateManagement
+{
+    class UnitTestMenuScreen : MenuScreen
+    {
+
+        public UnitTestMenuScreen() : base("Unit Test Menu")
+        {
+
+            MenuEntry gregTests = new MenuEntry("Greg Tests");
+            MenuEntry scottTests = new MenuEntry("Scott Tests");
+            MenuEntry yinTests = new MenuEntry("Yin Tests");
+            MenuEntry jesseTests = new MenuEntry("Jesse Tests");
+            MenuEntry exitMenuEntry = new MenuEntry("Back");
+
+            // Hook up menu event handlers.
+            gregTests.Selected += GotoGregTests;
+            scottTests.Selected += GotoScottTests;
+            yinTests.Selected += GotoYinTests;
+            jesseTests.Selected += GotoJesseTests;
+
+            exitMenuEntry.Selected += OnCancel;
+
+            MenuEntries.Add(gregTests);
+            MenuEntries.Add(scottTests);
+            MenuEntries.Add(yinTests);
+            MenuEntries.Add(jesseTests);
+            MenuEntries.Add(exitMenuEntry);
+       }
+
+
+        /// <summary>
+        /// Event handler for when the Options menu entry is selected.
+        /// </summary>
+        void GotoGregTests(object sender, EventArgs e)
+        {
+            ScreenManager.AddScreen(new GregTestMenu());
+        }
+
+        /// <summary>
+        /// Event handler for when the Options menu entry is selected.
+        /// </summary>
+        void GotoScottTests(object sender, EventArgs e)
+        {
+            ScreenManager.AddScreen(new ScottTestMenu());
+        }
+
+        /// <summary>
+        /// Event handler for when the Options menu entry is selected.
+        /// </summary>
+        void GotoYinTests(object sender, EventArgs e)
+        {
+            ScreenManager.AddScreen(new YinTestMenu());
+        }
+
+        /// <summary>
+        /// Event handler for when the Options menu entry is selected.
+        /// </summary>
+        void GotoJesseTests(object sender, EventArgs e)
+        {
+            ScreenManager.AddScreen(new JesseTestMenu());
+        }
+        
+
+       /// <summary>
+        /// Event handler for when the Options menu entry is selected.
+        /// </summary>
+        void exitMenuEntry(object sender, EventArgs e)
+        {
+            ScreenManager.AddScreen(new OptionsMenuScreen());
+        }
+
+    }
+}
