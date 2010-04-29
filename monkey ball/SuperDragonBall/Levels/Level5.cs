aa1c7b9d67ed21c4e4fb6f6d06d663fe30593cbd
skohn <skohn@usc.edu>
2010-04-29 07:24:03 +0000

    added in a fifth level

---
+++ monkey ball/SuperDragonBall/Levels/Level5.cs
@@ -0,0 +1,95 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Net;
+using Microsoft.Xna.Framework.Storage;
+using SuperDragonBall;
+using SuperDragonBall.Actors;
+
+namespace SuperDragonBall.Levels
+{
+    /// <summary>
+    /// This is a game component that implements IUpdateable.
+    /// </summary>
+    public class Level5 : LevelData
+    {
+        public Level5(Game game, GameplayScreen host)
+            : base(game, host)
+        {
+
+            startingLocation = new Vector3(1f, 35f, 1f);
+            //make a few planes
+
+            CollisionLevelPiece currentPlane;
+            currentPlane = new CollisionLevelPiece(game, host, "checker_plane_3");
+            currentPlane.scale = 15;
+            currentPlane.position += new Vector3(0f, 0f, 0);
+            planes.Add(currentPlane);
+
+            MovingLevelPiece movingPlane;
+            movingPlane = new MovingLevelPiece(game, host, "checker_plane_3", new Vector3(300, 0f, 0), 85f);
+            movingPlane.scale = 15;
+            movingPlane.position += new Vector3(-150f, 0f, -360f);
+            planes.Add(movingPlane);
+
+            movingPlane = new MovingLevelPiece(game, host, "checker_plane_3", new Vector3(300, 0f, 0), -85f);
+            movingPlane.scale = 15;
+            movingPlane.position += new Vector3(150f, 0f, -720f);
+            planes.Add(movingPlane);
+
+            movingPlane = new MovingLevelPiece(game, host, "checker_plane_3", new Vector3(300, 0f, 0), 85f);
+            movingPlane.scale = 15;
+            movingPlane.position += new Vector3(-150f, 0f, -1080f);
+            planes.Add(movingPlane);
+
+            movingPlane = new MovingLevelPiece(game, host, "checker_plane_3", new Vector3(300, 0f, 0), -85f);
+            movingPlane.scale = 15;
+            movingPlane.position += new Vector3(150f, 0f, -1440f);
+            planes.Add(movingPlane);
+
+
+
+            //goal
+            goal = new GoalObject(game, host);
+            goal.position += new Vector3(0, 10, -1440);
+            goal.scale = 10;
+
+            //collectables
+
+
+
+
+
+
+        }
+
+        /// <summary>
+        /// Allows the game component to perform any initialization it needs to before starting
+        /// to run.  This is where it can query for any required services and load content.
+        /// </summary>
+        public override void Initialize()
+        {
+            // TODO: Add your initialization code here
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// Allows the game component to update itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        public override void Update(GameTime gameTime)
+        {
+            // TODO: Add your update code here
+
+            base.Update(gameTime);
+        }
+    }
+}
